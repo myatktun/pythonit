5d4ecd6f07420dddb86aa8aa04586ed9f4c550cb
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-08-03 18:14:12 +0630

    perf(extract_data): split string with regex
    extract_data.py : split string of exiftool output with regex, create
                      scoped book/note object at the start of loop instead
                      of mutating a single object, check for title when
                      searching for isbn

--- extract_data.py
+++ extract_data.py
@@ -2,54 +2,65 @@ import subprocess
 import json
 import requests
 import os
+import re
 
 HOME = os.environ['HOME']
 
 def cleanNoteData():
-    data = subprocess.run(f"exiftool -FileName -Directory -FileModifyDate {HOME}/Documents/Vimwiki/*/*.*", shell=True, capture_output=True, text=True).stdout.strip("\n").split("\n")
+    data = subprocess.run(f"exiftool -FileName -Directory -FileModifyDate {HOME}/Documents/Vimwiki/*/*.*", shell=True, capture_output=True, text=True)\
+        .stdout\
+        .strip("\n")
+    data = re.split(r'\n=|\n\s', data)
     del data[-1]
     note_arr = []
-    note_obj = {}
+
     for item in data:
-        if item.startswith("="):
-            note_obj = {}
-            note_arr.append(note_obj)
-            continue
-        [key, value] = item.split(":", 1)
-        if "name" in key.lower():
-            note_obj["name"] = value.split(".")[0].strip()
-            continue
-        if key.startswith("Directory"):
-            category = value.split("/")[5].strip()
-            note_obj["category"] = category
-            continue
-        if "modification" in key.lower():
-            note_obj["dateAdded"] = value.strip()
-            continue
+        note_obj = {}
+        for line in item.strip().split("\n"):
+            if line.startswith("="):
+                continue
+            [key, value] = line.split(":", 1)
+            if "name" in key.lower():
+                note_obj["name"] = value.split(".")[0].strip()
+                continue
+            if key.startswith("Directory"):
+                category = value.split("/")[5].strip()
+                note_obj["category"] = category
+                continue
+            if "modification" in key.lower():
+                note_obj["dateAdded"] = value.strip().split()[0]
+                continue
+
+        note_arr.append(note_obj)
+
     return note_arr
 
 def cleanBookData():
-    data = subprocess.run(f"exiftool -BookName -Author -ISBN -FileModifyDate {HOME}/Documents/Books/*/kindle/*.*", shell=True, capture_output=True, text=True).stdout.strip("\n").split("\n")
+    data = subprocess.run(f"exiftool -BookName -Author -ISBN -FileModifyDate {HOME}/Documents/Books/*/kindle/*.*", shell=True, capture_output=True, text=True)\
+        .stdout\
+        .strip("\n")
+    data = re.split(r'\n=|\n\s', data)
     del data[-1]
     book_arr = []
-    book_obj = {}
 
     for item in data:
-        if item.startswith("="):
-            book_obj = {}
-            category = item.split("/")[5].strip()
-            book_obj["category"] = category
-            book_arr.append(book_obj)
-            continue
+        book_obj = {}
+        for line in item.strip().split("\n"):
+            if line.startswith("="):
+                category = line.split("/")[5].strip()
+                book_obj["category"] = category
+                continue
 
-        [key, value] = item.split(":", 1)
-        if key.startswith("Book"):
-            book_obj["name"] = value.strip()
-            continue
-        if key.startswith("Create"):
-            book_obj["dateAdded"] = value.strip()
-            continue
-        book_obj[key.strip().lower()] = value.strip()
+            [key, value] = line.split(":", 1)
+            if key.startswith("Book"):
+                book_obj["name"] = value.strip()
+                continue
+            if "modification" in key.lower():
+                book_obj["dateAdded"] = value.strip().split()[0]
+                continue
+            book_obj[key.strip().lower()] = value.strip()
+
+        book_arr.append(book_obj)
 
     return book_arr
 
@@ -73,12 +84,12 @@ def get_isbn(book):
     if r["numFound"]:
         for doc in r["docs"]:
             cover_found = False
-            if book["name"] in doc["title"]:
+            if ((book["name"] in doc["title"]) or (doc["title"] in book["name"])):
                 for seed in doc["seed"]:
                     if(seed.startswith("/books")):
                         bookUrl = f"https://openlibrary.org{seed}.json"
                         bookReq = requests.get(bookUrl).json()
-                        if ("covers" in bookReq) and (book["name"] in bookReq["title"]):
+                        if ("covers" in bookReq) and (bookReq["title"] in book["name"]):
                             print(f"========== getting OLID for {book['name']} ==========")
                             if not "isbn" in book:
                                 print(f"========== assigning ISBN for {book['name']} ==========")
@@ -93,6 +104,7 @@ def get_isbn(book):
 def writeData(title, data_arr):
     json_object = json.dumps(data_arr,indent=4)
     with open(f"{title}Data.json", "w") as outfile:
+        print(f"========== writing {title} data to file ==========")
         outfile.write(json_object)
 
 def main():
