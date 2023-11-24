35ad328487bb1f286f38df3db1ae9667710c4de4
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-11-24 16:24:44 +0630

    feat(sync_files_with_s3): sync files based on last modified
    Need to pipe commands instead of using "shell=True". Need to learn more
    about how subprocess works.
    sync_files_with_s3.py : add function to set sync directories based on
                            last modified time, aws s3 cli command use
                            "--size-only" to sync only if size differs

--- sync_files_with_s3.py
+++ sync_files_with_s3.py
@@ -1,6 +1,4 @@
-import subprocess
-import sys
-import os
+import subprocess, sys, os
 
 HOME = os.environ['HOME']
 excludePattern = "*.md"
@@ -17,10 +15,18 @@ def checkDryRun():
 
     return True
 
+def determineTime(localDir, s3Bucket):
+    command1 = f"find {localDir} -type f -exec stat -c \"%y %n\" {{}} + | sort -r | head -n 1 | awk '{{print $1 \" \" $2}}'"
+    command2 = f"aws s3 ls {s3Bucket} --recursive | sort | tail -n 1 | awk '{{print $1 \" \" $2}}'"
+    localTime = subprocess.run(command1, stdout=subprocess.PIPE, shell=True).stdout.decode("utf-8")
+    s3Time = subprocess.run(command2, stdout=subprocess.PIPE, shell=True).stdout.decode("utf-8")
+    return(localTime, s3Time)
+
 def setSyncDirs():
     localDir = f"{HOME}/Documents/Vimwiki"
     s3Bucket = "s3://projectxnotes"
-    if "--download" in sys.argv:
+    localTime, s3Time = determineTime(localDir, s3Bucket)
+    if (localTime < s3Time):
         print("Syncing files from s3 bucket to local")
         return (s3Bucket, localDir)
 
@@ -29,7 +35,7 @@ def setSyncDirs():
 
 dryRun = checkDryRun()
 source, destination = setSyncDirs()
-command = ["aws", "s3", "sync", source, destination, "--exclude", excludePattern, "--include", includePattern]
+command = ["aws", "s3", "sync", source, destination, "--exclude", excludePattern, "--include", includePattern, "--size-only"]
 
 if (dryRun):
     command.append("--dryrun")
