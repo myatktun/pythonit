362b1c7f13f3dd933711007775d2f86ee10d4a23
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-11-30 22:24:51 +0630

    perf(sync_files_with_s3): use argparse and dotenv
    sync_files_with_s3.py : use argparse to check various optional flags,
                            add "main()", remove global variables, instead
                            pass necessary arguments to functions

--- sync_files_with_s3.py
+++ sync_files_with_s3.py
@@ -1,31 +1,47 @@
-import subprocess, sys, os
+import subprocess
+import os
+import argparse
+from dotenv import load_dotenv
 
-HOME = os.environ['HOME']
-excludePattern = "*.md"
-includePattern = "*/*.md"
 
-def checkDryRun():
-    if "--dryrun" in sys.argv:
-        dryRunIndex = sys.argv.index("--dryrun") + 1;
-        if (dryRunIndex < len(sys.argv)) and (sys.argv[dryRunIndex].lower() in ("true", "false")):
-            return sys.argv[dryRunIndex].lower() == "true"
+def checkDryRun(args):
+    if args.dryrun is None:
+        return True
 
-        print("Provide a valid value for --dryrun")
-        exit(1)
+    return args.dryrun
 
-    return True
 
-def determineTime(localDir, s3Bucket):
+def getLastModifiedTimes(localDir, s3Bucket):
     command1 = f"find {localDir} -type f -exec stat -c \"%y %n\" {{}} + | sort -r | head -n 1 | awk '{{print $1 \" \" $2}}'"
     command2 = f"aws s3 ls {s3Bucket} --recursive | sort | tail -n 1 | awk '{{print $1 \" \" $2}}'"
+
     localTime = subprocess.run(command1, stdout=subprocess.PIPE, shell=True).stdout.decode("utf-8")
     s3Time = subprocess.run(command2, stdout=subprocess.PIPE, shell=True).stdout.decode("utf-8")
-    return(localTime, s3Time)
 
-def setSyncDirs():
-    localDir = f"{HOME}/Documents/Vimwiki"
-    s3Bucket = "s3://projectxnotes"
-    localTime, s3Time = determineTime(localDir, s3Bucket)
+    return (localTime, s3Time)
+
+
+def createArgumentParser():
+    parser = argparse.ArgumentParser("Sync note files between local directory and s3 bucket")
+    parser.add_argument("--dryrun", help="sync files or only dryrun (default: dryrun)", action=argparse.BooleanOptionalAction)
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--download", help="sync files from s3 bucket to local", action="store_true")
+    group.add_argument("--upload", help="sync files from local to s3 bucket", action="store_true")
+
+    return parser.parse_args()
+
+
+def getSyncDirs(localDir, s3Bucket, args):
+    if (args.download):
+        print("Syncing files from s3 bucket to local")
+        return (s3Bucket, localDir)
+    elif (args.upload):
+        print("Syncing files from local to s3 bucket")
+        return (localDir, s3Bucket)
+
+    localTime, s3Time = getLastModifiedTimes(localDir, s3Bucket)
+
     if (localTime < s3Time):
         print("Syncing files from s3 bucket to local")
         return (s3Bucket, localDir)
@@ -33,11 +49,30 @@ def setSyncDirs():
     print("Syncing files from local to s3 bucket")
     return (localDir, s3Bucket)
 
-dryRun = checkDryRun()
-source, destination = setSyncDirs()
-command = ["aws", "s3", "sync", source, destination, "--exclude", excludePattern, "--include", includePattern, "--size-only"]
 
-if (dryRun):
-    command.append("--dryrun")
+def main():
 
-subprocess.run(command)
+    load_dotenv()
+
+    args = createArgumentParser()
+    dryRun = checkDryRun(args)
+
+    localDir = os.environ['HOME'] + "/" + os.environ['LOCAL_DIR']
+    s3Bucket = os.environ['S3_BUCKET']
+
+    source, destination = getSyncDirs(localDir, s3Bucket, args)
+
+    excludePattern = "*.md"
+    includePattern = "*/*.md"
+
+    command = ["aws", "s3", "sync", source, destination, "--exclude", excludePattern, "--include", includePattern, "--size-only"]
+
+    if (dryRun):
+        command.append("--dryrun")
+
+    subprocess.run(command)
+
+
+if __name__ == "__main__":
+
+    main()
