d188b0928c6d79a27f5a850e5d9af32eeaba3cbe
Myat Kyaw Tun <myatktun2k@gmail.com>
2024-03-13 22:49:01 +0630

    feat(markdown_to_html): sync html files with s3 bucket
    Need refactor. Only call syn_files_with_s3 module once it is modified
    to sync files if given source and destination.
    markdown_to_html.py : main() accept list of files to convert, upload
                          flag and args, spawn process to run "s3 sync",
                          exit if converting index.md result to None

--- markdown_to_html.py
+++ markdown_to_html.py
@@ -1,39 +1,78 @@
-import pandoc
-from pandoc.types import *
-import glob
-import re
-from dotenv import load_dotenv
 import os
+import pandoc
+import re
+import sys
+from dotenv import load_dotenv
+from pandoc.types import *
+from subprocess import run as subprocess_run
 
-load_dotenv()
 
-LOCAL_DIR = os.environ['HOME'] + "/" + os.environ['LOCAL_DIR']
-MARKDOWN_DIR = f"{LOCAL_DIR}/*/*.md"
-INDEX_FILE = f"{LOCAL_DIR}/index.md"
+def main(files_to_convert, upload, args):
+    load_dotenv()
 
-with open(INDEX_FILE, "r", encoding="utf-8") as input_file:
-    html_file = f"{re.split('[/ .]', INDEX_FILE)[-2]}.html"
-    text = input_file.read()
-    doc = pandoc.read(text, format="markdown_mmd")
+    dry_run = check_dry_run(args)
 
-    for elt in pandoc.iter(doc[1]):
-        if isinstance(elt, Link):
-            elt[-1] = (f"{elt[-1][0]}.html", '')
+    LOCAL_DIR = os.environ['HOME'] + "/" + os.environ['LOCAL_DIR']
+    INDEX_FILE = f"{LOCAL_DIR}/index.md"
 
-    html = pandoc.write(
-        doc, format="html", file=f"html/{html_file}",
-        options=["--template=html/template.html"]
-    )
+    for i in range(len(files_to_convert)):
+        files_to_convert[i] = "/".join([LOCAL_DIR, files_to_convert[i]])
 
-for file in glob.glob(MARKDOWN_DIR):
-    with open(file, "r", encoding="utf-8") as input_file:
-        [category, html_file] = re.split('[/ .]', file)[-3:-1]
-        html_file = "".join([html_file, ".html"])
-        os.makedirs(f"html/{category}", exist_ok=True)
-        with open(f"html/{category}/{html_file}", "w",
-                  encoding="utf-8") as output_file:
-            text = input_file.read()
-            doc = pandoc.read(text, format="markdown_mmd")
-            html = pandoc.write(doc, format="html", options=[
-                                "--template=html/template.html"])
-            output_file.write(html)
+    generate_index(INDEX_FILE)
+    generate_html(files_to_convert)
+
+    source = os.environ['PWD'] + "html"
+    destination = "s3://notes-html1"
+
+    if upload:
+        print("Syncing html files from local to s3 bucket")
+
+    if not upload:
+        print("Syncing html files from s3 bucket to local")
+        source, destination = destination, source
+
+    command = ["aws", "s3", "sync", source, destination, "--size-only"]
+
+    if dry_run:
+        command.append("--dryrun")
+
+    subprocess_run(command)
+
+
+def check_dry_run(args):
+    if args.dryrun is None:
+        return True
+
+    return args.dryrun
+
+
+def generate_index(index_file):
+    with open(index_file, "r", encoding="utf-8") as input_file:
+        html_file = f"{re.split('[/ .]', index_file)[-2]}.html"
+        text = input_file.read()
+        doc = pandoc.read(text, format="markdown_mmd")
+
+        if doc is None:
+            sys.exit("Pandoc cannot convert file")
+
+        for elt in pandoc.iter(doc[1]):
+            if isinstance(elt, Link):
+                elt[-1] = (f"{elt[-1][0]}.html", '')
+
+        pandoc.write(doc, format="html", file=f"html/{html_file}",
+                     options=["--template=html/template.html"])
+
+
+def generate_html(markdown_dir):
+    for file in markdown_dir:
+        with open(file, "r", encoding="utf-8") as input_file:
+            [category, html_file] = re.split('[/ .]', file)[-3:-1]
+            html_file = "".join([html_file, ".html"])
+            os.makedirs(f"html/{category}", exist_ok=True)
+            with open(f"html/{category}/{html_file}", "w",
+                      encoding="utf-8") as output_file:
+                text = input_file.read()
+                doc = pandoc.read(text, format="markdown_mmd")
+                html = pandoc.write(doc, format="html", options=[
+                                    "--template=html/template.html"])
+                output_file.write(html)
