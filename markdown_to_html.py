c646d86ab1c7753d4f1639a530959c5ff251534d
Myat Kyaw Tun <myatktun2k@gmail.com>
2024-03-15 19:55:19 +0630

    refactor(markdown_to_html): only convert files
    markdown_to_html.py : only convert files to html. do not sync with s3.

--- markdown_to_html.py
+++ markdown_to_html.py
@@ -4,46 +4,16 @@ import re
 import sys
 from dotenv import load_dotenv
 from pandoc.types import *
-from sync_files_with_s3 import sync_files
 
 
-def main(md_files: list[str], *, dryrun=True):
+def convert_md_to_html(files_to_convert: list[str]):
     load_dotenv()
-
-    upload = check_upload(md_files[0])
-
-    files_to_convert: list[str] = [
-        "/".join(f.rsplit('/', 2)[-2:]) for f in md_files]
 
     LOCAL_DIR = os.environ['HOME'] + "/" + os.environ['LOCAL_DIR']
     INDEX_FILE = f"{LOCAL_DIR}/index.md"
 
-    files_to_convert = ["/".join([LOCAL_DIR, f]) for f in files_to_convert]
-
     generate_index(INDEX_FILE)
     generate_html(files_to_convert)
-
-    (source, destination) = get_sync_dirs(upload)
-
-    output = sync_files(source, destination, dryrun=dryrun)
-
-    print(output)
-
-
-def get_sync_dirs(upload: bool) -> tuple[str, str]:
-    LOCAL_HTML_DIR = os.environ['PWD'] + "html"
-    S3_BUCKET = os.environ['S3_HTML_BUCKET']
-
-    if upload:
-        print("Syncing html files from local to s3 bucket")
-        return (LOCAL_HTML_DIR, S3_BUCKET)
-
-    print("Syncing html files from s3 bucket to local")
-    return (S3_BUCKET, LOCAL_HTML_DIR)
-
-
-def check_upload(f: str) -> bool:
-    return f.split(':', 1)[0].split(' ')[-1].lower() == "upload"
 
 
 def generate_index(index_file: str):
