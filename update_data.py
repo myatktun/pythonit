2e8013d44d7c678a5b19d6e00ec31b689c73fa39
Myat Kyaw Tun <myatktun2k@gmail.com>
2024-03-15 19:27:50 +0630

    refactor(update_data): move sync_md_files()
    update_data.py : move sync_md_files() as its own module. add type hints

--- update_data.py
+++ update_data.py
@@ -1,48 +1,23 @@
 import argparse
-import os
-from dotenv import load_dotenv
 from markdown_to_html import main as convert_files
-from subprocess import Popen, PIPE
-from sync_files_with_s3 import sync_files
+from sync_markdown_files import sync_md_files
 
 
 def main():
-
-    load_dotenv()
     args = create_argument_parser()
-
-    md_files = sync_md_files(args)
-
-    upload = md_files[0].split(
-        ':', 1)[0].split(' ')[-1].lower() == "upload"
-
-    files_to_convert = []
-    for o in md_files:
-        file = "/".join(o.rsplit('/', 2)[-2:])
-        files_to_convert.append(file)
-
-    convert_files(files_to_convert, upload, args)
-
-
-def sync_md_files(args) -> list[str]:
-
     dryrun = check_dry_run(args)
-
-    LOCAL_DIR = os.environ['HOME'] + "/" + os.environ['LOCAL_DIR']
-    S3_BUCKET = os.environ['S3_BUCKET']
-
-    source, destination = get_sync_dirs(LOCAL_DIR, S3_BUCKET, args)
-
-    exclude_pattern = "*.md"
-    include_pattern = "*/*.md"
-
-    md_files = sync_files(source, destination, dryrun=dryrun,
-                          exclude=exclude_pattern, include=include_pattern)
-
-    return md_files.strip().split('\n')
+    md_files = sync_md_files(args, dryrun=dryrun)
+    convert_files(md_files, dryrun=dryrun)
 
 
-def create_argument_parser():
+def check_dry_run(args) -> bool:
+    if args.dryrun is None:
+        return True
+
+    return args.dryrun
+
+
+def create_argument_parser() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         "Sync files between local directory and s3 bucket")
 
@@ -63,70 +38,6 @@ def create_argument_parser():
         action="store_true")
 
     return parser.parse_args()
-
-
-def check_dry_run(args):
-    if args.dryrun is None:
-        return True
-
-    return args.dryrun
-
-
-def get_sync_dirs(local_dir, s3_bucket, args):
-    if args.download:
-        print("Syncing markdown files from s3 bucket to local")
-        return (s3_bucket, local_dir)
-    elif args.upload:
-        print("Syncing markdown files from local to s3 bucket")
-        return (local_dir, s3_bucket)
-
-    local_time, s3_time = get_last_modified_times(local_dir, s3_bucket)
-
-    if local_time == s3_time:
-        print("All files are in sync")
-        exit(0)
-
-    if local_time < s3_time:
-        print("Syncing markdown files from s3 bucket to local")
-        return (s3_bucket, local_dir)
-
-    print("Syncing markdown files from local to s3 bucket")
-    return (local_dir, s3_bucket)
-
-
-def get_last_modified_times(local_dir, s3_bucket):
-    command1 = ["find", f"{local_dir}", "-mindepth", "2", "-type",
-                "f", "-exec", "stat", "-c", "%y", "{}", "+"]
-
-    command2 = ["aws", "s3", "ls", f"{s3_bucket}", "--recursive"]
-
-    local_time = get_time(command1)
-    s3_time = get_time(command2)
-
-    return (local_time.split(".")[0], s3_time)
-
-
-def get_time(command):
-    time_list = Popen(command, stdout=PIPE)
-    assert time_list.stdout is not None
-
-    sorted_time_list = Popen(
-        ["sort", "-r"], stdin=time_list.stdout, stdout=PIPE)
-    time_list.stdout.close()
-    assert sorted_time_list.stdout is not None
-
-    top_row = Popen(["head", "-n", "1"],
-                    stdin=sorted_time_list.stdout, stdout=PIPE)
-    sorted_time_list.stdout.close()
-    assert top_row.stdout is not None
-
-    first_column = Popen(
-        ["awk", '{print $1 " " $2}'], stdin=top_row.stdout, stdout=PIPE)
-    top_row.stdout.close()
-
-    latest_time = first_column.communicate()[0].decode("utf-8")
-
-    return latest_time.strip()
 
 
 if __name__ == "__main__":
