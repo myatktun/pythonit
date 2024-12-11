bb8349bace30d85e910bcb2c7875fc7f62368703
Myat Kyaw Tun <myatktun2k@gmail.com>
2024-12-11 15:07:44 +0800

    refactor(modules): separate file converting and syncing modules

---
+++ modules/__init__.py
@@ -0,0 +1,7 @@
+from .file_converter.markdown_to_rst import convert_md_to_rst
+from .file_converter.markdown_to_html import convert_md_to_html
+from .file_converter.rst_to_html import convert_rst_to_html
+
+from .sync_files.sync_markdown_files import sync_markdown
+from .sync_files.sync_html_files import sync_html
+from .sync_files.sync_template_files import sync_templates
--- sync_files/__init__.py
+++
@@ -1,3 +0,0 @@
-from .sync_markdown_files import sync_markdown
-from .sync_html_files import sync_html
-from .sync_template_files import sync_templates
