c7f17f952fc9133031f4e97a9bcccd9991a4199a
Myat Kyaw Tun <myatktun2k@gmail.com>
2025-02-22 12:57:15 +0800

    refactor(file_converter): remove rst_to_html
    __init__.py    : remove convert_rst_to_html()
    rst_to_html.py : remove this as it is only using "make" command

--- src/modules/file_converter/__init__.py
+++ src/modules/file_converter/__init__.py
@@ -1,4 +1,3 @@
 from .markdown_to_rst import md_to_rst
-from .rst_to_html import convert_rst_to_html
 
-__all__ = ["md_to_rst", "convert_rst_to_html"]
+__all__ = ["md_to_rst"]
--- src/modules/file_converter/rst_to_html.py
+++
@@ -1,9 +0,0 @@
-from subprocess import run as subprocess_run
-import os
-
-
-def convert_rst_to_html():
-    # use "make" to generate html with Sphinx
-    command = ["make", "-C", os.environ["MAKE_DIRECTORY"], "html"]
-
-    subprocess_run(command)
